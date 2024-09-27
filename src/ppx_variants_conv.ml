4bab70c8ab2a2632fc75656009fa537a541bb527
Patrick Ferris <patrick@sirref.org>
2024-09-27 11:29:36 +0100

    Prepare for 5.2 AST bump

--- src/ppx_variants_conv.ml
+++ src/ppx_variants_conv.ml
@@ -488,7 +488,7 @@ module Gen_str = struct
           let body =
             if Variant_constructor.is_gadt v
             then locally_abstract_match loc cases ~variant_ty
-            else pexp_function ~loc cases
+            else pexp_function_cases ~loc cases
           in
           [%stri let [%p pvar ~loc name] = [%e body] [@@warning "-4"]]
         in
@@ -697,7 +697,7 @@ module Gen_str = struct
     let body =
       if List.exists variants ~f:Variant_constructor.is_gadt
       then locally_abstract_match loc cases ~variant_ty
-      else pexp_function ~loc cases
+      else pexp_function_cases ~loc cases
     in
     [%stri let to_rank = [%e body]]
   ;;
@@ -709,7 +709,7 @@ module Gen_str = struct
     let body =
       if List.exists variants ~f:Variant_constructor.is_gadt
       then locally_abstract_match loc cases ~variant_ty
-      else pexp_function ~loc cases
+      else pexp_function_cases ~loc cases
     in
     [%stri let to_name = [%e body]]
   ;;
